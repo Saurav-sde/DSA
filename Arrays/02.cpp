2f0804b986455186f6d1435e9a30665be86e90c2
Saurav-sde <saurav.sde.cs@gmail.com>
2024-10-16 03:25:24 +0530

    max Subarray sum  Kadane algo

---
+++ Arrays/02.cpp
@@ -0,0 +1,59 @@
+// Max SUbarray sum 
+#include<iostream>
+using namespace std;
+
+void maxSubArraySum1(int *arr,int n)
+{
+    int maxSum = INT8_MIN;
+    for(int start=0;start<n;start++)
+    {
+        for(int end = start;end<n;end++)
+        {
+            int currSum = 0;
+            for(int i=start;i<=end;i++)
+                currSum += arr[i];
+            maxSum = max(maxSum,currSum);  
+        }
+    }
+    cout<<maxSum;
+}
+
+// better approach
+void maxSubArraySum2(int *arr,int n)
+{
+    int maxSum = INT8_MIN;
+    for(int start=0;start<n;start++)
+    {
+        int currSum = 0;
+        for(int end = start;end<n;end++)
+        {
+            currSum += arr[end];
+            maxSum = max(maxSum,currSum);  
+        }
+    }
+    cout<<maxSum;
+}
+
+//KADANE'S ALGORITHM
+void maxSubArraySum3(int *arr,int n)
+{
+    int maxSum = INT8_MIN;
+    int currSum = 0;
+    for(int i=0;i<n;i++)
+    {
+        currSum += arr[i];
+        maxSum = max(maxSum,currSum);
+        if(currSum < 0)
+            currSum = 0;
+    }
+    cout<<"Max SubArray Sum is "<<maxSum;
+}
+
+int main()
+{
+    int arr[6] = {2,-3,6,-5,4,2};
+    int n = sizeof(arr)/sizeof(int);
+    maxSubArraySum3(arr,n);
+    return 0;
+
+}
